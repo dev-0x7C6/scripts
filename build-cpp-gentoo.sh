8a4fac0857f8a0d18ce3fc814515fea26e72bc74
Bartłomiej Burdukiewicz <dev.strikeu@gmail.com>
2016-01-25 00:08:44 +0100

    Updated cpp builder script, added new build options.

--- build-cpp-gentoo.sh
+++ build-cpp-gentoo.sh
@@ -2,26 +2,31 @@
 
 NAME="compiler-performance-platform"
 REPO="https://github.com/dev-0x7C6/${NAME}.git"
-COUNT=1000
-
-GCC_COUNT=$(gcc-config -l | wc -l)
-GCC_QMAKE="/usr/lib/qt5/bin/qmake"
-CLANG_QMAKE="/opt/qt/5.5.1-linux-clang/bin/qmake"
+COUNT=$1
+COUNT=${COUNT:=10}
 
 CXXFLAGS_MATCH="-std=c++14 -march=native -O3 -pipe -fomit-frame-pointer"
 CXXFLAGS_ARRAY=(
 	"-std=c++14 -march=native -O3"
 	"-std=c++14 -march=native -O2"
 	"-std=c++14 -march=native -O1"
+	"-std=c++14 -march=native -Os"
+	"-std=c++14 -march=native -Ofast"
+	"-std=c++14 -O3"
+	"-std=c++14 -O2"
+	"-std=c++14 -O1"
+	"-std=c++14 -Os"
+	"-std=c++14 -Ofast"
+)
+QMAKE_ARRAY=(
+	"/usr/lib/qt5/bin/qmake"
+	"/opt/qt/5.5.1-linux-clang/bin/qmake"
 )
 
 compile() {
-	pushd /tmp
-	mkdir tests
-	pushd tests
-
-	git clone $REPO $NAME
-	pushd $NAME
+	local workdir="/tmp/${NAME}"
+	git clone $REPO $workdir
+	pushd $workdir
 	git reset --hard
 	git clean -d -f -x
 
@@ -29,27 +34,21 @@ compile() {
 	cp -f cflags.pri.bak cflags.pri
 	$1 "${NAME}.pro"
 	make all -j8
-
 	./$NAME $COUNT
 
 	popd
-	popd
-	popd
 }
 
-
-for i in $(seq 1 $GCC_COUNT)
+for gcc_id in $(seq 1 $(gcc-config -l | wc -l))
 do
-	gcc-config -f $i
+	gcc-config -f $gcc_id
 	source /etc/profile
 
-	for j in "${CXXFLAGS_ARRAY[@]}"
+	for qmake_bin in "${QMAKE_ARRAY[@]}"
 	do
-		compile $GCC_QMAKE "${j}"
+		for cxxflags in "${CXXFLAGS_ARRAY[@]}"
+		do
+			compile "${qmake_bin}" "${cxxflags}" 
+		done
 	done
-done
-
-for j in "${CXXFLAGS_ARRAY[@]}"
-do
-	compile $CLANG_QMAKE "${j}"
 done
